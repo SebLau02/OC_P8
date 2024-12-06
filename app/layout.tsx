e93be0f405f6264920164da9dca675623971da70
Sébastien Lau <sebastien.lau2021@gmail.com>
2024-12-06 21:33:22 +0100

    add fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" suppressHydrationWarning className="h-full">
       <body
         className={`${RobotoMono.variable} ${CutiveMono.variable} font-sans h-full`}
       >
